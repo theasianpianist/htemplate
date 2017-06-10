49d459c0c79f92aa9a28919453686ee37cde4104
Lawrence Lee <lfqlee314@gmail.com>
2017-06-10 13:14:43 -0400

    feat: implement file input

--- src/main/java/HTemplate.java
+++ src/main/java/HTemplate.java
@@ -1,8 +1,42 @@
-package main.java;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * Created by theasianpianist on 6/10/17.
  */
 public class HTemplate {
-    
+
+    public static void main(String[] args){
+        System.out.print("Please enter the template file path: ");
+        Scanner input = new Scanner(System.in);
+        String templatePath = input.nextLine(); //Location of the template file
+        String template = getTemplate(templatePath); //String containing text of the template file
+        if (template.equals("")) {
+            System.out.println("Empty template file");
+        }
+        System.out.println(template);
+    }
+
+    private static String getTemplate(String filePath){
+        String str = "";
+        try {
+            File file = new File(filePath);
+            FileInputStream fis = new FileInputStream(file);
+            byte[] data = new byte[(int) file.length()];
+            fis.read(data);
+            fis.close();
+
+            str = new String(data, "UTF-8");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found");
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return str;
+    }
 }
