81118f201f131f43bd4f1bada43708f2bda9b11c
Lawrence Lee <lfqlee314@gmail.com>
2017-06-15 10:12:37 -0400

    feature: implement the rest of the stuff

--- src/main/java/HTemplate.java
+++ src/main/java/HTemplate.java
@@ -1,7 +1,4 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
@@ -33,10 +30,36 @@ public class HTemplate {
             System.out.println("Empty template file");
             System.exit(0);
         }
-        ArrayList<Integer> templateMarkerLocations = getMarkerLocations(template);
         ArrayList<File> contentFiles = getDirectoryFiles(contentPath);
-        for (File content : contentFiles) {
-
+        ArrayList<Integer> templateMarkerLocations = getMarkerLocations(template);
+        for (File file : contentFiles) {
+            ArrayList<Integer> contentMarkerLocations = getMarkerLocations(getFileContents(file.getPath()));
+            if (contentMarkerLocations.size() == templateMarkerLocations.size()){
+                String newFile = template;
+                for (int i = contentMarkerLocations.size() - 1; i >= 0; i--){ //Loop backwards because inserting content messes up later marker indices
+                    String toBeInserted;
+                    if (i == contentMarkerLocations.size() - 1){ //if processing last marker location, then substring without an end index
+                        toBeInserted = getFileContents(file.getPath()).substring(contentMarkerLocations.get(i) + marker.length() + 1);
+                        toBeInserted = toBeInserted.substring(0, toBeInserted.length() - 1);
+                    }
+                    else{
+                        toBeInserted = getFileContents(file.getPath()).substring(contentMarkerLocations.get(i) + marker.length() + 1, contentMarkerLocations.get(i + 1) - 1); //offset start and end indices by 1 to account for newline characters
+                    }
+                    newFile = newFile.substring(0, templateMarkerLocations.get(i)) + toBeInserted + newFile.substring(templateMarkerLocations.get(i) + marker.length(), newFile.length());
+                }
+                String oldFileName = file.getName().substring(0, file.getName().length() - 4); //grabs original filename minus .htp extension"
+                String newFileName = oldFileName + ".html";
+                try{
+                    PrintWriter writer = new PrintWriter(contentPath + "/" + newFileName, "UTF-8");
+                    writer.print(newFile);
+                    writer.close();
+                } catch (IOException e) {
+                    System.out.println("Your file could not be written to!");
+                }
+            }
+            else{
+                System.out.println("Template markers and content markers do not match up, skipping file " + file.getPath());
+            }
         }
 
     }
